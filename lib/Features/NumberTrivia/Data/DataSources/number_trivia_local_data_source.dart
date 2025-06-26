cd280109e75581a7a3812711234fb3b0a8acd540
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-26 15:14:19 +0200

    Tutorial 8

--- lib/Features/NumberTrivia/Data/DataSources/number_trivia_local_data_source.dart
+++ lib/Features/NumberTrivia/Data/DataSources/number_trivia_local_data_source.dart
@@ -1,6 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tutorial_1/Core/Error/exceptions.dart';
+
 import 'package:tutorial_1/Features/NumberTrivia/Data/Models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia ();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const key = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({
+    required this.sharedPreferences,
+  });
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(key);
+    if(jsonString != null){
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return sharedPreferences.setString(key, json.encode(triviaToCache.toJson()));
+  }
 }
