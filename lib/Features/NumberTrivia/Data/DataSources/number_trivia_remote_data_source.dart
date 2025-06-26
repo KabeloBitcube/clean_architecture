cb184c95e1c024a870dc5ce6f8b698de72f09300
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-26 15:54:21 +0200

    Tutorial 9

--- lib/Features/NumberTrivia/Data/DataSources/number_trivia_remote_data_source.dart
+++ lib/Features/NumberTrivia/Data/DataSources/number_trivia_remote_data_source.dart
@@ -1,6 +1,46 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:tutorial_1/Core/Error/exceptions.dart';
 import 'package:tutorial_1/Features/NumberTrivia/Data/Models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    final response = await client.get(
+      Uri.parse('http://numbersapi.com/$number'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200){
+       return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+    else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async{
+    final response = await client.get(
+      Uri.parse('http://numbersapi.com/random'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200){
+       return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+    else {
+      throw ServerException();
+    }
+  }
 }
