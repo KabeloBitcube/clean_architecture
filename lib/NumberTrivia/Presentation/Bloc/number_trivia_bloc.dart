cc8e7b7535cd272c4f9673f5074fabe3e337325f
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-27 10:29:08 +0200

    Tutorial 12

--- lib/NumberTrivia/Presentation/Bloc/number_trivia_bloc.dart
+++ lib/NumberTrivia/Presentation/Bloc/number_trivia_bloc.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first, constant_identifier_names
+// ignore_for_file: public_member_api_docs, sort_constructors_first, constant_identifier_names, void_checks
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:tutorial_1/Core/Error/failures.dart';
 
 import 'package:tutorial_1/Core/Util/input_converter.dart';
 import 'package:tutorial_1/NumberTrivia/Domain/Entities/number_trivia.dart';
@@ -12,7 +13,8 @@ part 'number_trivia_state.dart';
 
 const String SERVER_FAILURE_MESSAGE = "Server Failure";
 const String CACHE_FAILURE_MESSAGE = "Cache Failure";
-const String INVALID_INPUT_FAILURE_MESSAGE = "Invalid Input - The number must be a positive integer or zero";
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    "Invalid Input - The number must be a positive integer or zero";
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
@@ -24,16 +26,47 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     this.getRandomNumberTrivia,
     this.inputConverter,
   ) : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
+    on<NumberTriviaEvent>((event, emit) async* {
       if (event is GetTriviaForConcreteNumber) {
         final inputEither = inputConverter.stringToUnsignedInteger(
           event.numberString,
         );
 
-        emit(
-          inputEither.fold((failure) => Error(message: INVALID_INPUT_FAILURE_MESSAGE), (integer) => throw UnimplementedError()),
+        yield* inputEither.fold(
+          (failure) async* {
+            yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+          },
+          (integer) async* {
+            yield Loading();
+            final failureOrTrivia = await getConcreteNumberTrivia(
+              Params(number: integer),
+            );
+            yield failureOrTrivia.fold(
+              (failure) => Error(message: _failureMessage(failure)),
+              (trivia) => Loaded(trivia: trivia),
+            );
+          },
         );
       }
+      else if (event is GetTriviaForRandomNumber){
+        yield Loading();
+            final failureOrTrivia = await getRandomNumberTrivia(NoParams());
+            yield failureOrTrivia.fold(
+              (failure) => Error(message: _failureMessage(failure)),
+              (trivia) => Loaded(trivia: trivia),
+            );
+      }
     });
+  }
+
+  String _failureMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure():
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure():
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error';
+    }
   }
 }
